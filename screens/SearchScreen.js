f069f034d07b2f530b43151e6c4bf4c46a8fb47b
Fayiz-A <68380206+Fayiz-A@users.noreply.github.com>
2021-01-14 16:19:49 +0530

    added the search screen

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,13 +1,137 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import { Text, View, FlatList, StyleSheet, TextInput, TouchableOpacity } from 'react-native';
+import database from '../config'
+
+
 
 export default class SearchScreen extends React.Component {
+   constructor(props) {
+      super(props)
+      this.state = {
+         allTransactions: [],
+         lastVisibleTransaction: null,
+         search: ''
+      }
+   }
 
+   fetchMoreTransactions = async () => {
+      var text = this.state.search.toUpperCase()
+      var enteredText = text.split("")
+
+
+      if (enteredText[0]) {
+         const query = await database.collection("transactions").where('bookID', '==', text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+         query.docs.map((doc) => {
+            this.setState({
+               allTransactions: [...this.state.allTransactions, doc.data()],
+               lastVisibleTransaction: doc
+            })
+         })
+      }
+      else if (enteredText[0].toUpperCase() === 'S') {
+         const query = await database.collection("transactions").where('bookID', '==', text).startAfter(this.state.lastVisibleTransaction).limit(10).get()
+         query.docs.map((doc) => {
+            this.setState({
+               allTransactions: [...this.state.allTransactions, doc.data()],
+               lastVisibleTransaction: doc
+            })
+         })
+      }
+   }
+
+   searchTransactions = async (text) => {
+      var enteredText = text.split("")
+      if (enteredText[0]) {
+         const transaction = await database.collection("transactions").where('bookID', '==', text).get()
+         transaction.docs.map((doc) => {
+            this.setState({
+               allTransactions: [...this.state.allTransactions, doc.data()],
+               lastVisibleTransaction: doc
+            })
+         })
+         console.log(`All transactions: ${this.state.allTransactions}`)
+      }
+      else if (enteredText[0].toUpperCase() === 'S') {
+         const transaction = await database.collection('transactions').where('studentID', '==', text).get()
+         transaction.docs.map((doc) => {
+            this.setState({
+               allTransactions: [...this.state.allTransactions, doc.data()],
+               lastVisibleTransaction: doc
+            })
+         })
+      }
+   }
+
+   componentDidMount = async () => {
+      const query = await database.collection("transactions").limit(10).get()
+      query.docs.map((doc) => {
+         this.setState({
+            allTransactions: [],
+            lastVisibleTransaction: doc
+         })
+      })
+   }
    render() {
-      return(
-         <View>
-            <Text>Search</Text>
+      return (
+         <View style={styles.container}>
+            <View style={styles.searchBar}>
+               <TextInput
+                  style={styles.bar}
+                  placeholder="Enter Book Id or Student Id"
+                  onChangeText={(text) => { this.setState({ search: text }) }} />
+               <TouchableOpacity
+                  style={styles.searchButton}
+                  onPress={() => { this.searchTransactions(this.state.search) }}
+               >
+                  <Text>Search</Text>
+               </TouchableOpacity>
+            </View>
+            <FlatList
+               data={this.state.allTransactions}
+               renderItem={({ item }) => (
+                  <View style={{ borderBottomWidth: 2 }}>
+                     <Text>{"Book Id: " + item.bookID}</Text>
+                     <Text>{"Student id: " + item.studentID}</Text>
+                     <Text>{"Transaction Type: " + item.transactionType}</Text>
+                     <Text>{"Date: " + item.date}</Text>
+                  </View>
+               )}
+               keyExtractor={(item, index) => index.toString()}
+               onEndReached={this.fetchMoreTransactions}
+               onEndReachedThreshold={0.7}
+            />
          </View>
-      )
+      );
    }
 }
+
+
+const styles = StyleSheet.create({
+   container: {
+      flex: 1,
+      marginTop: 20
+   },
+   searchBar: {
+      flexDirection: 'row',
+      height: 40,
+      width: 'auto',
+      borderWidth: 0.5,
+      alignItems: 'center',
+      backgroundColor: 'white',
+
+   },
+   bar: {
+      borderWidth: 2,
+      height: 30,
+      width: 300,
+      paddingLeft: 10,
+   },
+   searchButton: {
+      borderWidth: 1,
+      height: 30,
+      width: 50,
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'green'
+   }
+})
